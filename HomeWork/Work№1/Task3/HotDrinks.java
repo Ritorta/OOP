0e2247c339f99f9c88a55f8baab23466dd7881b0
Ritorta <Meddesu@yandex.ru>
2023-09-26 13:21:07 +0500

    Almost Done Work 3

--- HomeWork/Work№1/Task3/HotDrinks.java
+++ HomeWork/Work№1/Task3/HotDrinks.java
@@ -7,7 +7,6 @@ public class HotDrinks extends DrinksMachine
     {
 
         super(name, volume, temperature, cost);
-        //TODO Auto-generated constructor stub
 
     }
 
--- HomeWork/Work№1/Task3/Main.java
+++ HomeWork/Work№1/Task3/Main.java
@@ -10,5 +10,18 @@ import java.util.List;
 
 public class Main 
 {
-    
+    public static void main(String[] args)   
+    { 
+        DrinksMachine DrinkProductCoffe = new DrinksMachine("Coffe", 0.4, 96, 89.99);
+        DrinksMachine DrinkProductTea = new DrinksMachine("Tea", 0.5, 120, 34.99);
+        DrinksMachine DrinkProductWatter = new DrinksMachine("Watter", 0.9, 0, 59.99);
+        
+        List<DrinksMachine> addProduct = new ArrayList<>(List.of(DrinkProductCoffe, DrinkProductTea, DrinkProductWatter));
+
+        VendingMachine vendingproduct = new VendingMachine();
+        vendingproduct.getProducts(addProduct);
+        vendingproduct.SearchName("Coffe");
+        vendingproduct.SearchVolume(0.9);
+        vendingproduct.SearchTemperature(120);
+    }
 }
