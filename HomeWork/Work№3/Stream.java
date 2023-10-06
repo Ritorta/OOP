ec840209988a6265e50213e4af25f78f9100f610
Ritorta <Meddesu@yandex.ru>
2023-10-06 20:54:12 +0500

    Create Stream and release Iterable

--- HomeWork/Work№3/Stream.java
+++ HomeWork/Work№3/Stream.java
@@ -1,4 +1,37 @@
-public class Stream 
-{
-    
-}
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+
+    public class Stream implements Iterator<StudentGroup>,Iterable
+    {
+        private List<StudentGroup> group = new ArrayList<>();
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() 
+        {
+            return currentIndex < group.size();
+
+        }
+
+        @Override
+        public StudentGroup next() 
+        {
+            if(hasNext())
+                {
+                    return group.get(currentIndex++);
+                }
+
+            throw new IllegalArgumentException();
+        }
+
+        @Override
+        public Iterator<StudentGroup> iterator() 
+        {
+            return group.iterator();
+            
+        }
+        
+
+    }
+
--- HomeWork/Work№3/StudentGroup.java
+++ HomeWork/Work№3/StudentGroup.java
@@ -1,4 +1,39 @@
 public class StudentGroup 
 {
+    protected String nameGroup;
+    protected int numberStudent;
+
+    public StudentGroup(String nameGroup, int numberStudent) 
+    {
+        this.nameGroup = nameGroup;
+        this.numberStudent = numberStudent;
+        
+    }
+
+    public String getNameGroup() 
+    {
+        return nameGroup;
+
+    }
+
+    public void setNameGroup(String nameGroup) 
+    {
+        this.nameGroup = nameGroup;
+
+    }
+
+    public int getNumberStudent() 
+    {
+        return numberStudent;
+
+    }
+    
+    public void setNumberStudent(int numberStudent) 
+    {
+        this.numberStudent = numberStudent;
+
+    }
+    
+
     
 }
