67375ae06e6bfb098750c07d92be6c1eb50764b4
Ritorta <Meddesu@yandex.ru>
2023-10-11 15:17:10 +0500

    add controler logic

--- Work№4/TeacherController.java
+++ Work№4/TeacherController.java
@@ -1,4 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeacherController 
 {
+    private List<Teacher> teacherList = new ArrayList<>();
+    private TeacherServes teacherServes;
+    private TeacherView teacherView;
+
+    public TeacherController(TeacherServes teacherServes, TeacherView teacherView) 
+    {
+        this.teacherServes = teacherServes;
+        this.teacherView = teacherView;
+
+    }
+
+    public void addTeacher(String name, String object)
+    {
+        Teacher createteacher = new Teacher(name, object);
+
+        if(teacherServes.validateCheck(createteacher))
+        {
+            teacherList.add(createteacher);
+        }
+
+        else
+        {
+            System.out.println("Error: incorrect data teacher!");
+        }
+
+    }
     
+    public void editTeacher(int index, String name, String object)
+    {
+        if(index >= 0 && index < teacherList.size())
+        {
+            Teacher curenTeacher = teacherList.get(index);
+            curenTeacher.setName(name);
+            curenTeacher.setObject(object);
+        }
+
+        else
+        {
+            System.out.println("Error: incorrect index teacher!");
+        }
+
+    }
+
+    public void TeacherView()
+    {
+        teacherView.viewTeacherList(teacherList);
+        
+    }
 }
--- Work№4/TeacherView.java
+++ Work№4/TeacherView.java
@@ -1,8 +1,19 @@
+import java.util.List;
+
 public class TeacherView 
 {
     public void viewTeacher(Teacher teacher)
     {
         System.out.println("Name Teacher: " + teacher.getName() + "Leads object: " + teacher.getObject());
 
+    }
+
+    public void viewTeacherList(List<Teacher> teachers)
+    {
+        for(Teacher teacher : teachers)
+        {
+            viewTeacher(teacher);
+            System.out.println();
+        }
     }
 }
