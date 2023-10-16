a93285a9323cff067cb04fc7b2dc1cedbd95f945
Ritorta <Meddesu@yandex.ru>
2023-10-16 17:58:28 +0500

    ALmost Done HW6

--- Work№6/Main.java
+++ Work№6/Main.java
@@ -4,32 +4,34 @@
 // усвоенных на семинаре. Нужно в проекте прокомментировать участки кода, 
 // которые рефакторим, какой принцип применяем и почему.
 
+// Импорт класса.
 import data.Student;
 import data.StudentGroup;
 import data.User;
 import view.StudentView;
 import controller.StudentController;
-
+// Создание класса Main. 
 public class Main {
     public static void main(String[] args) {
-        // Create StudentController object
+        // Создание StudentController для управления программой.
         StudentController<User> studentController = new StudentController<>(new StudentView());
 
-        // Create groups
+        // Создание групп студентов.
         studentController.createGroup("Group 1");
         studentController.createGroup("Group 2");
 
-        // Create students
+        // Создание студентов.
         Student student1 = new Student("John", "Doe", "Fox", (long) 1);
         Student student2 = new Student("Jane", "Smith", "Ojld", (long) 2);
 
-        // Add students to groups
+        // Получение групп студентов.
         StudentGroup<User> group1 = studentController.getGroups().get(0);
         StudentGroup<User> group2 = studentController.getGroups().get(1);
+        // Добавление групп студентов в другие группы.
         studentController.addStudentToGroup(group1, student1);
         studentController.addStudentToGroup(group2, student2);
-
-        // Send groups on console
+        // Вывод информации в консоль спомощью view
         studentController.sendGroupsOnConsole();
+
     }
 }
