8c26fedf2fec4817e3fc7388294bfe465d0335ba
Ritorta <Meddesu@yandex.ru>
2023-10-18 12:47:07 +0500

    controller add

--- Work№7/controller/CalculatorController.java
+++ Work№7/controller/CalculatorController.java
@@ -1,6 +1,48 @@
 package controller;
 
+import data.Calculator;
+import data.CalculatorComplexNumbers;
+import view.CalculatorView;
+
 public class CalculatorController 
 {
+    private view.CalculatorView view;
+    private data.Calculator calculator;
+    private data.CalculatorComplexNumbers num1, num2;
+
+    public CalculatorController(CalculatorView view, Calculator calculator, CalculatorComplexNumbers num1, CalculatorComplexNumbers num2) 
+    {
+        this.view = view;
+        this.calculator = calculator;
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public void AdditionOperation(Double add)
+    {
+        calculator.AdditionOperation(num1, num2);
+    }
+    
+    public void SubtractionOperation(Double Sub)
+    {
+        calculator.SubtractionOperation(num1, num2);
+    }
+
+    public void MultiplicationOperation(Double Mul)
+    {
+        calculator.MultiplicationOperation(num1, num2);
+    }
+
+    public void DivisionOperation(Double Div)
+    {
+        calculator.DivisionOperation(num1, num2);
+    }
+
+    public void View()
+    {
         
+    }
+
+
+
 }
--- Work№7/view/CalculatorView.java
+++ Work№7/view/CalculatorView.java
@@ -1,6 +1,43 @@
 package view;
 
+import java.util.Scanner;
+
+import data.CalculatorLogic;
+import service.CalculatorLoger;
+import service.CalculatorService;
+
 public class CalculatorView
 {
-    
+    class View 
+    {
+        private CalculatorService calculatorService;
+        private CalculatorLogic calculatorLogic;
+        private CalculatorLoger calculatorLoger;
+
+        // Конструктор класса
+        public View() 
+        {
+            calculatorService = new CalculatorService();
+        }
+
+        // Метод для отображения меню и выбора операции
+        public void showMenu() 
+        {
+            System.out.println("1. Сложение");
+            System.out.println("2. Вычитание");
+            System.out.println("3. Умножение");
+            System.out.println("4. Деление");
+            System.out.println("0. Выход");
+        }
+
+        // Метод для получения выбранной операции от пользователя
+        public int getOperationChoice() 
+        {
+            System.out.print("Выберите операцию (0-4): ");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Считываем символ новой строки после ввода числа
+            return choice;
+        }
+    }
 }
