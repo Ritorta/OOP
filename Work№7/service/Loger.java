414099a07d3609299a0d4b9212b70901171fb064
Ritorta <Meddesu@yandex.ru>
2023-10-18 10:50:47 +0500

    create Logs.txt and Loger add metod

--- Work№7/service/Loger.java
+++ Work№7/service/Loger.java
@@ -1,36 +1,98 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Formatter;
+import java.util.Scanner;
 
 public class Loger 
-{
-    private FileWriter fileWriter;
+{    
+    //Создание переменной и файла для чтения и записи.
+    File file = new File("Logs.txt");
+    FileWriter writer;
+    BufferedReader reader;
 
-    public void Logs(String fileName) throws IOException 
+    public Loger() 
     {
-        fileWriter = new FileWriter(fileName, true);
+        
     }
-
-    public void logOperation(String operation) 
+    // Создание метода "write", который принимает строковый аргумент "record". 
+    public void write(String record) 
     {
-        System.out.println("Logging operation: " + operation);
-        try {
-            fileWriter.write(operation + "\n");
-            fileWriter.flush();
-        } catch (IOException e) 
+        // Внутри метода происходит попытка записать значение "record" в файл "Logs.txt". 
+        try 
         {
-            System.out.println("Failed to write to log file: " + e.getMessage());
+            if (file.exists()) 
+            {
+                // Если файл уже существует, то открывается в режиме дозаписи.   
+                this.writer = new FileWriter(file, true);
+            }
+            // Иначе - создается новый файл.
+            else this.writer = new FileWriter(file);
+            // Затем записывается значение "record" и закрывается FileWriter. 
+            writer.write(record + '\n');
+            writer.close();
+        }
+        // Если происходит ошибка ввода-вывода, то выбрасывается исключение RuntimeException. 
+        catch (IOException e) 
+        {
+            throw new RuntimeException(e);
         }
     }
-
-    public void close() 
+    //Создание метода "erase", который очищает содержимое файла "Logs.txt".  
+    public void erase() 
     {
-        try {
-            fileWriter.close();
-        } catch (IOException e) 
+        // Создается новый объект Scanner для чтения файла.
+        Scanner scan = new Scanner("Logs.txt");
+        // Затем создается объект Formatter с файлом "Logs.txt", что позволяет записывать в него данные.
+        Formatter form = null;
+        try 
         {
-            System.out.println("Failed to close log file: " + e.getMessage());
+            form = new Formatter("Logs.txt");
+        } 
+        // Если возникает ошибка FileNotFoundException, то выбрасывается исключение RuntimeException.   
+        catch (FileNotFoundException e) 
+        {
+            throw new RuntimeException(e);
         }
+        // В цикле while происходит чтение файла построчно и затем вызывается метод format() у объекта Formatter, 
+        // который записывает пустую строку, что приводит к удалению соответствующей строки из файла. Затем закрывается Scanner. 
+        while (scan.hasNext()) 
+        {
+            form.format("");
+            scan.next();
+        }
+    }
+    // Создание метода "read", который считывает содержимое файла "Logs.txt" и возвращает его в виде строки.   
+    public String read() 
+    {
+        String result = "";
+        // Создается объект BufferedReader с файлом "Logs.txt" для чтения данных.
+        try 
+        {
+            this.reader = new BufferedReader(new FileReader(file));
+            // В цикле "while" происходит чтение файла построчно с использованием метода readLine().
+            while (true) 
+            {
+                String temp = reader.readLine();
+                // Если достигнут конец файла (temp == null), то цикл прерывается.
+                if (temp == null) break;
+                // В остальных случаях строка "temp" добавляется к переменной "result" с добавлением символа новой строки.
+                result += temp + "\n";
+            }
+            // Затем закрывается BufferedReader.
+            reader.close();
+        } 
+        // Если возникает ошибка ввода-вывода, то выбрасывается исключение RuntimeException.
+        catch (IOException e) 
+        {
+            throw new RuntimeException(e);
+        }
+        // В конце метода возвращается содержимое файла в виде строки.
+        return result;
     }
 }
